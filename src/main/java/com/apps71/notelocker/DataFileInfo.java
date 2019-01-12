6d8e1a8ac73a760e8bc5dcb951266088dd4ecec5
chiku71 <chiku71@live.com>
2019-01-12 19:24:59 +1100

    Added new element to Security Questions

--- src/main/java/com/apps71/notelocker/DataFileInfo.java
+++ src/main/java/com/apps71/notelocker/DataFileInfo.java
@@ -3,6 +3,12 @@ package com.apps71.notelocker;
 
 class DataFileInfo
 {
+    // -------------------------------------------------------------------------------------------------------------------------------------------------
+    // Encryption Key to Decrypt User Credentials. Don't change it.
+    final private static String USER_DATA_ENCRYPTION_KEY = null;
+    final private static String BACKUP_DATA_ENCRYPTION_KEY = null;
+    // --------------------------------------------------------------------------------------------------------------------------------------------------
+
     // Stores All Notes created by User.
     final private static String APP_DATA_FILE = "ApplicationData.mnl";
     // Stores User Credentials.
@@ -12,20 +18,18 @@ class DataFileInfo
 
     // Tool Welcome Note
     final private static  String WELCOME_NOTE_FILE = "Welcome_Note.txt";
-    // Encryption Key to Decrypt User Credentials. Don't change it.
-    final private static String USER_DATA_ENCRYPTION_KEY = null;
-    final private static String BACKUP_DATA_ENCRYPTION_KEY = null;
 
+    final protected static String WRITE_YOUR_OWN_SECIRITY_QUESTION_TXT = "Have your own security question.";
+    
     final private static String[] SECRET_QUESTIONS_SPINNER_OPTIONS = {
                                                     "Select your secret question ...",
                                                     "What is your favorite movie ?",
-                                                    "What is the last name of the teacher who gave you your first failing grade ?",
                                                     "What was the name of the hospital where you were born ?",
                                                     "What is your pet's name ?",
                                                     "What is your mother's maiden name ?",
                                                     "What is your best friend's last name ?",
                                                     "Where was your father born ?",
-                                                    "What is the middle name of your oldest child ?"
+                                                    WRITE_YOUR_OWN_SECIRITY_QUESTION_TXT
     };
 
     protected static String getAppDataFileName() { return APP_DATA_FILE; }
